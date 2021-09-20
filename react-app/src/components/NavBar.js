2c7a21ea43f79329789552da997565cda37cce13
Tasha Syarifah <tashadifa@gmail.com>
2021-09-20 23:58:24 +0700

    Add authentication

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,23 +1,64 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+    import React, { useState, useEffect } from 'react';
+    import { NavLink } from 'react-router-dom';
 
-const NavBar = (props) => {
-  return (
-    <div className="column is-2">
-      <nav className="menu">
-        <p className="menu-label">Menu</p>
-        <ul className="menu-list">
-          <NavLink to="/products" activeClassName="active-link">
-            Products
-          </NavLink>
-          <NavLink to="/about" activeClassName="active-link">
-            About
-          </NavLink>
-        </ul>
-        {props.children}
-      </nav>
-    </div>
-  );
-};
+    const NavBar = (props) => {
+      const providers = ['twitter', 'github', 'aad'];
+      const redirect = window.location.pathname;
+      const [userInfo, setUserInfo] = useState(); 
 
-export default NavBar;
+      useEffect(() => {
+        (async () => {
+          setUserInfo(await getUserInfo());
+        })();
+      }, []);
+
+      async function getUserInfo() {
+        try {
+          const response = await fetch('/.auth/me');
+          const payload = await response.json();
+          const { clientPrincipal } = payload;
+          return clientPrincipal;
+        } catch (eror) {
+          console.eror('No profile could be found');
+          return undefined;
+        }
+      }
+      return (
+        <div className="column is-2">
+          <nav className="menu">
+            <p className="menu-label">Menu</p>
+            <ul className="menu-list">
+              <NavLink to="/products" activeClassName="active-link">
+                Products
+              </NavLink>
+              <NavLink to="/about" activeClassName="active-link">
+                About
+              </NavLink>
+            </ul>
+            {props.children}
+          </nav>
+          <nav className="menu auth">
+            <p className="menu-label">Auth</p>
+            <div className="menu-list auth">
+              {!userInfo && providers.map((provider) => (
+                <a key={provider} href={`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`}>
+                  {provider}
+                </a>
+              ))}
+              {userInfo && <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>logout</a>}
+            </div>
+          </nav>
+          {userInfo && (
+            <div>
+              <div className="user">
+                <p>Welcome</p>
+                <p>{userInfo && userInfo.userDetails}</p>
+                <p>{userInfo && userInfo.identityProvider}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      );
+    };
+
+    export default NavBar;
